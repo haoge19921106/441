f88e77846feed1fdf276c065f561225488c526cc
hao19921106 <512569360@qq.com>
2016-12-01 22:24:31 +0800

    add

--- wangyi/js/wangyi.js
+++ wangyi/js/wangyi.js
@@ -3,13 +3,6 @@ $(document).ready(function() {
             anchors:['page1', 'page2', 'page3','page4', 'page5', 'page6','page7', 'page8', 'page9','page10'],
             scrollingSpeed: 2000,
             navigation: true,
-            onleave:function(index,nextIndex,direction){
-                if(index==1){
-               $(".section1").removeClass("active")
-                }
-               }
         });
-    setTimeout(function(){
-        $(".section1").addClass("active")
-    },500)
+
     })
